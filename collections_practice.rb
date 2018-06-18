91099077eee5357676161e6e58d01a33e71c9a3a
Jane Justiz <janejustiz@gmail.com>
2018-06-18 00:18:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,3 +13,26 @@ def sort_array_desc(array)
     end
   end
 end
+
+def sort_array_char_count
+  
+end
+
+def swap_elements
+
+end
+
+def reverse_array
+end
+
+def kesha_maker
+end
+
+def find_a
+end
+
+def sum_array
+end
+
+def add_s
+end
