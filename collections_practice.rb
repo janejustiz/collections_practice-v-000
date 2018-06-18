e8fc2fc98aef2d21c67c9a770ec825ae81550521
Jane Justiz <janejustiz@gmail.com>
2018-06-18 00:24:13 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,7 +15,15 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count
-
+  array.sort do |a , b|
+    if a.size == b.size
+      0
+    elsif a.size > b.size
+      -1
+    elsif a.size < b.size
+      1
+    end
+  end
 end
 
 def swap_elements
@@ -23,19 +31,19 @@ def swap_elements
 end
 
 def reverse_array
-  
+
 end
 
 def kesha_maker
-  
+
 end
 
 def find_a
-  
+
 end
 
 def sum_array
-  
+
 end
 
 def add_s
